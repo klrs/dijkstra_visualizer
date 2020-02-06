bfa439c5c97f24b3ce6bf5ac3b273f4db3a34e56
klrs <kllrssn@gmail.com>
2020-02-06 22:49:13 +0200

    moved script tags to head

--- klrs.github.io/js/main.js
+++ klrs.github.io/js/main.js
@@ -1,11 +1,13 @@
 (function() {
-  let grid = new Grid(10, 10, 25);
-  grid.init();
-  grid.draw();
+  window.onload = function() {
+    let grid = new Grid(10, 10, 25);
+    grid.init();
+    grid.draw();
 
-  let algorithm = new Djikstra(grid);
-  let findPathButton = document.getElementById("findPath");
-  findPathButton.addEventListener("click", function() {
-    grid.findPath(algorithm);
-  });
+    let algorithm = new Djikstra(grid);
+    let findPathButton = document.getElementById("findPath");
+    findPathButton.addEventListener("click", function() {
+      grid.findPath(algorithm);
+    });
+  };
 })();
